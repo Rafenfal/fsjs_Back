025babc6a98a61632b7f0bb97f8891ad1d97d499
alexandre Tahery <alexandre1.tahery@epitech.eu>
2021-09-19 14:13:22 +0200

    app.js

--- dist/src/app.js
+++ dist/src/app.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
+import express from ("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-import { Request, Response } from "express";
-
-const express = require("express");
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
-app.get("/", (req : Request, res : Response) => {
+
+app.get("/", (req: Request, res : Response) => {
   res.send("Hello World!");
 });
 
